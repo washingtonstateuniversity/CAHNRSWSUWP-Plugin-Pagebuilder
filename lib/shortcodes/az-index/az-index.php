97c8c7438ac2714c053eb0228aa10c172182feee
danialbleile <danbleile@gmail.com>
2018-07-23 11:09:30 -0700

    Fix indent spacing

--- lib/shortcodes/az-index/az-index.php
+++ lib/shortcodes/az-index/az-index.php
@@ -1,9 +1,7 @@
 <?php namespace CAHNRSWP\Plugin\Pagebuilder;
 
 ?><div class="cpb-az-index-column">
-<?php foreach ( $col_items as $index => $item ) :
-	$class = ( ! empty( $item['link'] ) ) ? ' has-link' : '';
-?><div class="cpb-az-index-column-item <?php echo esc_html( $class ); ?>">
+<?php foreach ( $col_items as $index => $item ) : $class = ( ! empty( $item['link'] ) ) ? ' has-link' : ''; ?><div class="cpb-az-index-column-item <?php echo esc_html( $class ); ?>">
 	<h3 class="cpb-az-index-column-item-title"><?php echo esc_html( $item['title'] ); ?></h3>
 	<?php if ( ! empty( $item['link'] ) ) :
 		?><div><a class="cpb-az-index-column-item-link" href="<?php echo esc_url( $item['link'] ); ?>">Visit: <?php echo esc_html( $item['title'] ); ?></a></div>
